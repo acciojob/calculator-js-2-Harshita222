f063359d930f062851762ddf25f58ba70750b8d6
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-13 10:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const display= documnet.getElementById('display')
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            display.value = "";
+        } else if (value === "⌫") {
+            display.value = display.value.slice(0, -1);
+        } else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        } else {
+            display.value += value;
+        }
+    });
+});
